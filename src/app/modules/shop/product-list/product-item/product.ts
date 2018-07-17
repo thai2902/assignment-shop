71076a9e3aa67d330d3ab88049d76201f66487f3
Thai Le <thai2902@gmail.com>
2018-07-18 02:09:18 +0700

    build github page

--- src/app/modules/shop/product-list/product-item/product.ts
+++
@@ -1,9 +0,0 @@
-export class Product {
-    constructor(
-        public id: string,
-        public picture: string,
-        public name: string,
-        public price: number,
-        public description: string
-    ) {}
-}
--- src/app/modules/shop/wish-list/wish-list-item/wish-list-item.ts
+++
@@ -1,8 +0,0 @@
-import { Product } from '../../product-list/product-item/product';
-
-export class WishlistItem {
-    constructor(
-        public product: Product,
-        public dateAdded: Date = new Date() ) {
-    }
-}
